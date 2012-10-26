86f820b38386d33269c1f819999456141a25d907
Kenny Ng <zqkng@stanford.edu>
2012-10-25 17:16:56 -0700

    Set relative path for TEMPALTE_DIRS

--- medicalme/settings.py
+++ medicalme/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Django settings for medicalme project.
 
 DEBUG = True
@@ -104,10 +106,13 @@ ROOT_URLCONF = 'medicalme.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'medicalme.wsgi.application'
 
+PROJECT_DIR = os.path.dirname(__file__)
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
